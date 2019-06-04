a96c6a492908eccc918f0a93947a9c432044b3a3
Prabath Siriwardena <prabath@wso2.com>
2019-06-03 21:51:05 -0700

    Create user_attr_based_idp.js

---
+++ user_attr_based_idp.js
@@ -0,0 +1,37 @@
+// system should have two roles pickup-admin and pickup-user
+// four steps engaged with the corresponding service provider: username/password (step-1), OTP over SMS (step-2), FIDO U2F (step-3), Is9445 as an IdP (step-4)
+
+var allowedRoles = ['pickup-admin', 'pickup-user'];
+var rolesForFido = ['pickup-admin'];
+var is9445IpdIP = ['192.168.1.2','192.168.1.3'];
+
+function onLoginRequest(context) {
+	var userip = context.request.ip;
+	if (is9445IpdIP.indexOf(userip) > -1 ) {
+		executeStep(4);
+	} else {
+		executeStep(1, {
+			onSuccess: function (context) {
+				var idp = context.steps[1].idp;
+				var user = context.steps[1].subject;
+				if (idp == 'LOCAL') {
+					var inAllowedRoles = hasAnyOfTheRoles(user, allowedRoles);
+					if (!inAllowedRoles) {
+						sendError('http://carpooling.com:8080/oidc-web-app-pickup/', {});
+					}
+					var useFido = hasAnyOfTheRoles(user, rolesForFido);
+
+					if (useFido) {
+						executeStep(3);
+					} else {
+						executeStep(2);
+					}
+				} else {
+					var firstName = user.remoteClaims.first_name;
+					var lastName = user.remoteClaims.last_name;
+					user.remoteClaims.full_name = firstName + ' ' + lastName;
+				}
+			}
+		});
+	}
+}
