7058c2c1319894630c15b45b25ac859b483beb69
Prabath Siriwardena <prabath@wso2.com>
2019-06-03 18:56:41 -0700

    Create rbac_with_multi_option.js

---
+++ rbac_with_multi_option.js
@@ -0,0 +1,35 @@
+var allowedRoles = ['pickup-admin', 'pickup-user'];
+var rolesForFido = ['pickup-admin'];
+
+function onLoginRequest(context) {
+
+	executeStep(1, {
+		onSuccess: function (context) {
+
+			var idp = context.steps[1].idp;
+			var user = context.steps[1].subject;
+
+			if (idp == 'LOCAL') {
+
+				var inAllowedRoles = hasAnyOfTheRoles(user, allowedRoles);
+
+				if (!inAllowedRoles) {
+					sendError('http://carpooling.com:8080/oidc-web-app-pickup/', {});
+				}
+
+				var useFido = hasAnyOfTheRoles(user, rolesForFido);
+
+				if (useFido) {
+					executeStep(3);
+				} else {
+					executeStep(2);
+				}
+
+			} else {
+				// don't do anything if the idp is not local
+			}
+		}
+
+	});
+
+}
