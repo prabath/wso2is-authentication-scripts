093d9001feac5f3e14cfcd447e26d7fc5c2402e9
Prabath Siriwardena <prabath@wso2.com>
2019-06-03 20:29:46 -0700

    Update ip_based_idp.js

--- ip_based_idp.js
+++ ip_based_idp.js
@@ -3,28 +3,19 @@ var rolesForFido = ['pickup-admin'];
 var boaip = '192.168.1.2';
 
 function onLoginRequest(context) {
-
 	var userip = context.request.ip;
-
 	if (userip == boaip) {
-
 		executeStep(4);
 	} else {
-
 		executeStep(1, {
 			onSuccess: function (context) {
-
 				var idp = context.steps[1].idp;
 				var user = context.steps[1].subject;
-
 				if (idp == 'LOCAL') {
-
 					var inAllowedRoles = hasAnyOfTheRoles(user, allowedRoles);
-
 					if (!inAllowedRoles) {
 						sendError('http://carpooling.com:8080/oidc-web-app-pickup/', {});
 					}
-
 					var useFido = hasAnyOfTheRoles(user, rolesForFido);
 
 					if (useFido) {
@@ -32,16 +23,12 @@ function onLoginRequest(context) {
 					} else {
 						executeStep(2);
 					}
-
 				} else {
 					var firstName = user.remoteClaims.first_name;
 					var lastName = user.remoteClaims.last_name;
 					user.remoteClaims.full_name = firstName + ' ' + lastName;
 				}
-
 			}
-
 		});
 	}
-
 }
